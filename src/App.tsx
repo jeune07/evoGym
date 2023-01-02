9b660254148af63cd16a66677a7d72c6be71273a
jeune007 <jeunewinsley9@gmail.com>
2023-01-01 23:04:00 -0300

    the navbar is entirely responsive

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid"
+import  Navbar from "@/scenes/navbar"
+import {SelectedPage} from "@/shared/types"
+
+
+
+function App() {
+  const[selectedPage,setSelectedPage]=useState<SelectedPage>(SelectedPage.Home) 
+  const[isTopOfPage, setIsTopOfPage]=useState<boolean>(true)
+  useEffect(() => {
+    const handleScroll=()=>{
+      if (window.scrollY==0){
+        setIsTopOfPage(true)
+        setSelectedPage(SelectedPage.Home)        
+      }
+      if (window.scrollY !== 0){
+        setIsTopOfPage(false)
+      }
+     } 
+     window.addEventListener("scroll", handleScroll)   
+     return()=> window.removeEventListener("scroll", handleScroll)
+     }, [])
+  
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar
+      isTopOfPage={isTopOfPage}
+      selectedPage={selectedPage}
+      setSelectedPage={setSelectedPage}
+      />
+    </div>
+  )
+}
+
+export default App
